cc03ba667d6bf1eadcf8c87a081c3438ee7cfed0
Chi Yu <cfyu@princeton.edu>
2018-07-06 19:34:22 -0400

    1. on page save button

--- save.js
+++ save.js
@@ -1,31 +1,54 @@
-var save = document.getElementById("save");
+var prevButton;
+var prevText;
 
-save.onclick = function () {
-    chrome.tabs.executeScript( {
-        code: "window.getSelection().toString();"
-    }, function(selection) {
-        document.getElementById("output").innerHTML = selection[0];
+document.addEventListener("click", function(){
+    var selection = window.getSelection();
+    var text = selection.toString();
+    console.log(text);
+    
+    // if a text is selected
+    if (text.length > 0 && prevText !== text) {
+        // remove the previous button
+        if (prevButton) prevButton.parentElement.removeChild(prevButton);
 
-        // store in the format -- date: word
-        var obj = {};
-        var key = getDate();
+        var node = selection.focusNode;
+        console.log(node.nodeValue);
 
-        // check if key exists
-        chrome.storage.sync.get(key, function(item) {
-            if (Object.keys(item).length === 0) {
-                obj[key] = [selection[0]];
-            } else {
-                item[key].push(selection[0]);
-                obj[key] = item[key];
-            }
-            
-            chrome.storage.sync.set(obj, function() {
-                chrome.extension.getBackgroundPage().console.log('You saved a word: ' + selection[0] + '!');
+        // create new button
+        var button = document.createElement("button");
+        var buttonText = document.createTextNode("click me!");
+        button.appendChild(buttonText);
+
+        node.parentElement.appendChild(button);
+        prevButton = button;
+        prevText = text;
+
+        // if the button is pressed, store the word
+        button.onclick = function() {
+            // store in the format -- date: word
+            var obj = {};
+            var key = getDate();
+
+            // check if key exists
+            chrome.storage.sync.get(key, function(item) {
+                if (Object.keys(item).length === 0) {
+                    obj[key] = [text];
+                } else {
+                    item[key].push(text);
+                    obj[key] = item[key];
+                }
+                
+                chrome.storage.sync.set(obj, function() {
+                    console.log('You saved a word: ' + text + '!');
+                })
             })
-        })
-    });
-}
 
+            // remove the button
+            button.innerHTML = "Saved!";
+            setTimeout(function(){button.parentElement.removeChild(button)}, 2000);
+        }
+    }
+})
 
 // get today's month and date
 function getDate() {
