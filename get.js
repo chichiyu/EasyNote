8484ca0d8a457ce31fcae5edbfd4254546c45915
Chi Yu <cfyu@princeton.edu>
2018-07-10 17:08:47 -0400

    1. Removed get button

--- get.js
+++ get.js
@@ -1,17 +1,14 @@
 var output = document.getElementById("output");
-var get = document.getElementById("get");
 
-get.onclick = function() {
-    chrome.storage.sync.get(null, function(results){
-        var text = "";
-        for (var date in results) {
-            chrome.extension.getBackgroundPage().console.log(date);
-            text += date + ":<br>";
-            for (var word of results[date]) {
-                text = text + word + "<br>"
-            }
+chrome.storage.sync.get(null, function(results){
+    var text = "";
+    for (var date in results) {
+        chrome.extension.getBackgroundPage().console.log(date);
+        text += date + ":<br>";
+        for (var word of results[date]) {
+            text = text + word + "<br>"
         }
-        output.innerHTML = text;
-        chrome.extension.getBackgroundPage().console.log("GOT");
-    })
-}
+    }
+    output.innerHTML = text;
+    chrome.extension.getBackgroundPage().console.log("GOT");
+})
